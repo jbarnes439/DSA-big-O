460c6f8791a648974f0658501fdd578f04b72e6a
Malik DeJean <mdejean@MacBook-Pro.local>
2020-10-27 13:09:55 -0700

    hanoi function

--- DSA-big-O.js
+++ DSA-big-O.js
@@ -19,3 +19,14 @@ function findRandomElement(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
 
+var hanoi = function (disc, src, aux, dest) {
+  if (disc > 0) {
+    hanoi(disc - 1, src, dest, aux);
+    console.log('Move disc ' + disc + ' from ' + src + ' to ' + dest);
+    hanoi(disc - 1, aux, src, dest);
+  }
+};
+
+hanoi(3, 'src', 'aux', 'dest');
+
+
